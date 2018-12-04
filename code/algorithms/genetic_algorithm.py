f06493ab48514d14b3125a2cb67470dbefc314d8
skyerguo <skyerguo97@gmail.com>
2018-12-04 01:10:15 +0100

    before pool

--- code/algorithms/genetic_algorithm.py
+++ code/algorithms/genetic_algorithm.py
@@ -12,15 +12,23 @@ sys.path.append('../')
 from classes.chip import chip
 import astar_spfa
 
+POOL_SIZE = 5000
+PARENT_SIZE = 50
 
-def load_pool(pool_size, parent_size, chipsize, chipgate, chipnetlist):
+
+def create_pool(pool_size, chipsize, chipgate, chipnetlist):
     total_wires = len(chipnetlist)
-    print(total_wires)
+    index = [[] for i in range(pool_size)]
 
     ans = 0
     for i in range(pool_size):
         current_chip = chip(chipsize, chipgate, chipnetlist)
         ans = astar_spfa.astar_spfa(current_chip)
+        current_chip.save("GApool/astar-%04d-%02d.json" % (i, ans))
+        print(i)
+
+
+# def load_pool(pool_size, parent_size):
 
 
 if __name__ == '__main__':
@@ -28,4 +36,5 @@ if __name__ == '__main__':
     chipgate = readjson("gatelists.json", 1)
     chipnetlist = readjson("netlists.json", 3)
 
-    load_pool(5000, 50, chipsize, chipgate, chipnetlist)
+    create_pool(POOL_SIZE, chipsize, chipgate, chipnetlist)
+    # load_pool(POOL_SIZE, PARENT_SIZE)
