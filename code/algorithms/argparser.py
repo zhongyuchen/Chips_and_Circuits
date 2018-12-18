da8a2d606f8dc3fde100f4ba176a1a01369ed9ba
Zhongyu Chen <zhongyuchen@yahoo.com>
2018-12-18 10:04:06 +0100

    hc parser

--- code/algorithms/argparser.py
+++ code/algorithms/argparser.py
@@ -1,10 +1,11 @@
 from algorithms.astar_spfa import AstarSpfa
 from algorithms.genetic_algorithm import GeneticAlgorithm
-
+from algorithms.hillclimber import HillClimber
 import sys
 sys.path.append('../')
 import argparse
 from classes.environment import Environment
+
 
 
 def argparser():
@@ -16,7 +17,8 @@ def argparser():
                         help='choose the netlist.')
 
     parser.add_argument('--algorithm', default="astar",
-                        choices=["astar", "genetic", "hillclimbing"],
+                        choices=["astar", "genetic", "hillclimbing",
+                                 "randomwalk", "hillclimbing_solution"],
                         help='the algorithm that is used.')
 
     parser.add_argument('--astar-complete', default=1, type=int,
@@ -32,8 +34,26 @@ def argparser():
     parser.add_argument('--genetic-generationSize', default=30, type=int,
                         help='The generation size for genetic algorithm')
 
-    args = parser.parse_args()
+    parser.add_argument('--steps', default=50, type=int,
+                        help='The steps for hill climber')
+    parser.add_argument('--amount', default=3, type=int,
+                        help='The amount of wires changed in a transformation for hill climber')
+    parser.add_argument('--retry', default=3, type=int,
+                        help='The allowed max retry in a step in hill climber')
+    parser.add_argument('--savechip', default=False, type=bool,
+                        help='Save the best chip in json file after hill climbing')
+    parser.add_argument('--showchip', default=False, type=bool,
+                        help='Show the best chip after hill climbing')
+    parser.add_argument('--result', default=True, type=bool,
+                        help='Show the hill climbing process')
+    parser.add_argument('--savechip_name', default="hillclimbing_bestchip.json", type=str,
+                        help='The name of the chip json file')
+    parser.add_argument('--showchip_name', default="hillclimbing_bestchip", type=str,
+                        help='The name of the chip plot')
+    parser.add_argument('--result_name', default="hillclimbing_result", type=str,
+                        help='The name of the plot of hill climbing process')
 
+    args = parser.parse_args()
 
     if args.algorithm != 'astar' and args.astar_complete != True:
         parser.error('--astar-complete can only be set when --algorithm=astar.')
@@ -48,13 +68,47 @@ def argparser():
         parser.error('--genetic-generationSize can only be set when --algorithm=genetic.')
 
     env = Environment(args.netlist)
+    steps = args.steps
+    amount = args.amount
+    retry = args.retry
+    savechip = args.savechip
+    showchip = args.showchip
+    result = args.result
+    savechip_name = args.savechip_name
+    showchip_name = args.showchip_name
+    result_name = args.result_name
 
-    algos = {"astar": AstarSpfa(env).run,
-             "genetic": GeneticAlgorithm(env).run}
+    algos = {
+        "astar": AstarSpfa(env).run,
+        "genetic": GeneticAlgorithm(env).run,
+        "hillclimbing":
+            HillClimber(env, steps=steps, amount=amount, retry=retry,
+                        save_chip=savechip, show_chip=showchip,
+                        show_lineplot=result,
+                        chip_filename=savechip_name,
+                        chip_plotname=showchip_name,
+                        lineplot_filename=result_name).hillclimbing,
+        "randomwalk":
+            HillClimber(env, steps=steps, amount=amount, retry=retry,
+                        save_chip=savechip, show_chip=showchip,
+                        show_lineplot=result,
+                        chip_filename=savechip_name,
+                        chip_plotname=showchip_name,
+                        lineplot_filename=result_name).randomwalk,
+        "hillclimbing_solution":
+            HillClimber(env, steps=steps, amount=amount, retry=retry,
+                        save_chip=savechip, show_chip=showchip,
+                        show_lineplot=result,
+                        chip_filename=savechip_name,
+                        chip_plotname=showchip_name,
+                        lineplot_filename=result_name).hillclimbing_solution,
+    }
 
     if args.algorithm == "astar":
         algos[args.algorithm](args.astar_complete)
     if args.algorithm == "genetic":
         algos[args.algorithm](args.genetic_poolSize, args.genetic_parentSize, args.genetic_generationSize)
+    else:
+        algos[args.algorithm]()
 
     parser.print_help()
